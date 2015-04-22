48d04f4971c66d405068301c67de827217c60ebf
HOMMA Takashi <hommatks@gmail.com>
2015-04-23 04:24:32 +0900

    VB2008だとActionをラムダで書けないからC#で実現する

--- RxTest.Test/Rx02/Rx0201Test.vb
+++ RxTest.Test/Rx02/Rx0201Test.vb
@@ -4,31 +4,21 @@ Imports NUnit.Framework
 Namespace Rx02
     Public Class Rx0201Test
 
-        <Test()> Public Sub Hoge()
-            Dim foo As Func(Of Integer, Object) = Function(x As Integer) New Object
-            Observable.Range(1, 5).Subscribe(New Action(Of Integer)( sub(x) console.write(x)))
-        End Sub
-
-        Private Shared Function ToAction(Of T)(ByVal arg1 As T, ByVal x As Func(Of T, Object)) As Action(Of T)
-            x.Invoke()
-
-        End Function
-
-        Private Shared Function ToFunc(Of T1, T2)(ByVal arg1 As T1, ByVal x As Action(Of T1)) As Func(Of T1, T2)
-            x.Invoke(arg1)
-            Return Nothing
-        End Function
-
-        Public Class Piyo(Of T)
-            Private ReadOnly fx As Func(Of T, Object)
-            Public Sub New(ByVal fx As Func(Of T, Object))
-                Me.fx = fx
-            End Sub
-
+        Private Class Piyo(Of T)
+            Public ReadOnly List As New List(Of T)
             Public Sub Compute(ByVal x As T)
-                fx.
+                List.Add(x)
             End Sub
         End Class
+
+        <Test()> Public Sub ObservableによるRx的コード()
+            Dim a As New Piyo(Of Integer)
+
+            Observable.Range(1, 5).Subscribe(AddressOf a.Compute)
+
+            Assert.That(a.List.Count, [Is].EqualTo(5))
+            Assert.That(Join(a.List.ConvertAll(Function(x) x.ToString).ToArray, ","), [Is].EqualTo("1,2,3,4,5"))
+        End Sub
 
     End Class
 End Namespace
