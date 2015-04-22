604188f7fb6bc8aee4036edaea7d54e15f3e2f1d
HOMMA Takashi <hommatks@gmail.com>
2015-04-23 04:39:05 +0900

    C3で同じテストを追加する

---
+++ RxC.Test/Rx0201Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RxC
+{
+    class Rx0201Test
+    {
+        [Test] public void ObservableによるRx的コード()
+        {
+            var result =new List<int>();
+            Observable.Range(1, 5).Subscribe(x => result.Add(x));
+            Assert.That(result.Count, Is.EqualTo(5));
+        }
+    }
+}
