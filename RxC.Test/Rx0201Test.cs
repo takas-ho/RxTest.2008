896e610f7237ea9cb9e2e8459902ea995c67e76a
HOMMA Takashi <hommatks@gmail.com>
2015-04-23 12:24:56 +0900

    通常パターンのテストを追加する

--- RxC.Test/Rx0201Test.cs
+++ RxC.Test/Rx0201Test.cs
@@ -14,6 +14,21 @@ namespace RxC
             var result =new List<int>();
             Observable.Range(1, 5).Subscribe(x => result.Add(x));
             Assert.That(result.Count, Is.EqualTo(5));
+            Assert.That(string.Join(",", result.ConvertAll(x => x.ToString()).ToArray()), 
+                Is.EqualTo("1,2,3,4,5"));
         }
+
+        [Test] public void Enumberableによるコード()
+        {
+            var result = new List<int>();
+            foreach (var x in Enumerable.Range(1, 5))
+            {
+                result.Add(x);                
+            }
+            Assert.That(result.Count, Is.EqualTo(5));
+            Assert.That(string.Join(",", result.ConvertAll(x => x.ToString()).ToArray()), 
+                Is.EqualTo("1,2,3,4,5"));
+        }
+
     }
 }
